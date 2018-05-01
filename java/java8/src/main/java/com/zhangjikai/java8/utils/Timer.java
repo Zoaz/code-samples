7b4bddd484b8429a99cc8dc524f8458096e65c3b
zhangjikai <zhangjk1993@gmail.com>
2018-05-01 17:15:23 +0800

    add parallel stream

---
+++ java/java8/src/main/java/com/zhangjikai/java8/utils/Timer.java
@@ -0,0 +1,22 @@
+package com.zhangjikai.java8.utils;
+
+/**
+ * @author Jikai Zhang
+ * @date 2018-05-01
+ */
+public class Timer {
+    
+    private static ThreadLocal<Long> startTimeLocal = new ThreadLocal<>();
+    
+    public static void start() {
+        long startTime = System.currentTimeMillis();
+        startTimeLocal.set(startTime);
+    }
+    
+    public static void end() {
+        long endTime = System.currentTimeMillis();
+        long usedTime = endTime - startTimeLocal.get();
+        System.out.println("运行时间：" + usedTime + "ms");
+        startTimeLocal.remove();
+    }
+}
